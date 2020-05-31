8e96c4d8665141501d759a0cba35f161471651b5
mohamedkush <mohamed.kechaou@alumni.fh-aachen.de>
2020-05-31 22:54:29 +0200

    rm carousel

--- tunrw-front/src/app/app.component.ts
+++ tunrw-front/src/app/app.component.ts
@@ -1,18 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  @ViewChild("owlCarousel", { static: false }) owlCarousel: ElementRef;
+
   title = 'tunrw-front';
   menuExpandFlag = false;
 
-  onMenuClick(){
+  ngOnInit(): void {
+  }
+
+
+  onMenuClick() {
     this.menuExpandFlag = !this.menuExpandFlag;
   }
-  onFBClick(){
+  onFBClick() {
     window.open("https://www.facebook.com/TunRW.ev/", "_blank");
+    this.owlCarousel.nativeElement.owlCarousel({
+      loop: true,
+      margin: 10,
+      nav: true,
+      responsive: {
+        0: {
+          items: 1
+        },
+        600: {
+          items: 3
+        },
+        1000: {
+          items: 5
+        }
+      }
+    });
+
   }
 }
