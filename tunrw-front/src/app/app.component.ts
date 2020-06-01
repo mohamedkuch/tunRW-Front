b088235d9ac38aaae2378bc2aaca2f78633c2bb7
mohamedkush <mohamed.kechaou@alumni.fh-aachen.de>
2020-06-01 02:50:11 +0200

    finish slider

--- tunrw-front/src/app/app.component.ts
+++ tunrw-front/src/app/app.component.ts
@@ -14,8 +14,8 @@ export class AppComponent implements OnInit {
   public slides = [
     { src: "https://images.unsplash.com/photo-1506919258185-6078bba55d2a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1115&q=80" },
     { src: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80" },
-    { src: "https://images.unsplash.com/photo-1506919258185-6078bba55d2a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1115&q=80" },
-    { src: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80" }
+    { src: "https://images.unsplash.com/photo-1519742866993-66d3cfef4bbd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=581&q=80" },
+    { src: "https://images.unsplash.com/photo-1488161628813-04466f872be2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80" }
   ];
 
   ngOnInit(): void {
--- tunrw-front/src/app/carousel/carousel.component.ts
+++ tunrw-front/src/app/carousel/carousel.component.ts
@@ -19,20 +19,62 @@ import {
 })
 export class CarouselComponent implements OnInit {
   @Input() slides;
-  constructor() { }
   currentSlide = 0;
+  sndSlide = -1;
+  thdSlide = -1;
+
+  constructor() {  }
   ngOnInit() {
+    console.log(this.slides.length);
+    if (this.slides.length > 1) this.sndSlide = 1;
+    if (this.slides.length > 2) this.thdSlide = 2;
+
+
   }
 
   onPreviousClick() {
     const previous = this.currentSlide - 1;
     this.currentSlide = previous < 0 ? this.slides.length - 1 : previous;
+
+    this.sndSlide = this.currentSlide + 1;
+    if(this.currentSlide == 2){
+      this.thdSlide = 0
+    }
+    else if(this.currentSlide == 3){
+      this.thdSlide = 1;
+      this.sndSlide = 0;
+    }
+    else {
+      this.thdSlide = this.currentSlide + 2;
+    }
+
+
     console.log("previous clicked, new current slide is: ", this.currentSlide);
   }
 
   onNextClick() {
     const next = this.currentSlide + 1;
     this.currentSlide = next === this.slides.length ? 0 : next;
+
+    this.sndSlide = this.currentSlide + 1;
+    if(this.currentSlide == 2){
+      this.thdSlide = 0
+    }
+    else if(this.currentSlide == 3){
+      this.thdSlide = 1;
+      this.sndSlide = 0;
+    }
+    else {
+      this.thdSlide = this.currentSlide + 2;
+    }
+
     console.log("next clicked, new current slide is: ", this.currentSlide);
+  }
+  onSwipeLeft(event) {
+    this.onNextClick();
+  }
+  onSwipeRight(event) {
+    this.onPreviousClick();
+
   }
 }
