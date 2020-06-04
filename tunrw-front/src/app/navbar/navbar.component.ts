d4ae0baf6b6724ee769f8f44f23d2f6f81476612
mohamedkush <mohamed.kechaou@alumni.fh-aachen.de>
2020-06-04 22:48:45 +0200

    fix general styles

--- tunrw-front/src/app/navbar/navbar.component.ts
+++ tunrw-front/src/app/navbar/navbar.component.ts
@@ -6,15 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-  menuExpandFlag = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  onMenuClick() {
-    this.menuExpandFlag = !this.menuExpandFlag;
-  }
 
 }
