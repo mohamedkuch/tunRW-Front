2ef985fc59d94a5a155ca83855df81c4a1298a9e
mohamed kechaou <medksbuss@gmail.com>
2020-09-01 00:27:25 +0200

    fixing arrows

--- tunrw-front/src/app/team/team.component.ts
+++ tunrw-front/src/app/team/team.component.ts
@@ -16,7 +16,7 @@ export class TeamComponent implements OnInit {
     { src: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80", name: "Lorem kechaou", position: "Poster" },
     { src: "https://images.unsplash.com/photo-1519742866993-66d3cfef4bbd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=581&q=80", name: "Test Omar", position: "Manager" },
     { src: "https://images.unsplash.com/photo-1488161628813-04466f872be2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80", name: "Hamza kechaou", position: "CTO" }
-  
+
   ];
 
   constructor() { }
