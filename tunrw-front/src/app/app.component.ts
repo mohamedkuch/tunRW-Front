b2d9cc98eed7b603bbfdf60d0462cccecf49d6b4
mohamed kechaou <medksbuss@gmail.com>
2020-09-01 15:37:44 +0200

    finish fixing navbar active link

--- tunrw-front/src/app/app.component.ts
+++ tunrw-front/src/app/app.component.ts
@@ -1,15 +1,44 @@
-import { Component, ViewChild, ElementRef, OnInit, HostListener } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  ElementRef,
+  OnInit,
+  HostListener,
+} from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent implements OnInit {
-  @HostListener('window:scroll', ['$event']) 
+  @ViewChild("cluster", { static: false }) cluster: ElementRef;
+
+  @HostListener("window:scroll", ["$event"])
   doSomething(event) {
-  }
-  ngOnInit(): void {
+    console.log("#", window.scrollY);
+
+    for (
+      let index = 0;
+      index < this.cluster.nativeElement.children.length;
+      index++
+    ) {
+      const element = this.cluster.nativeElement.children[index];
+      let navTeam = document.getElementById("navbar-menu").children;
+
+      if (window.scrollY > element.offsetTop)
+        navTeam[index].classList.add("active");
+
+      if (window.scrollY > element.offsetTop + element.children[0].clientHeight)
+        navTeam[index].classList.remove("active");
+
+      if (window.scrollY < element.offsetTop && index != 0)
+        navTeam[index].classList.remove("active");
+
+    }
+
   }
 
+  constructor() {}
+  ngOnInit(): void {}
 }
